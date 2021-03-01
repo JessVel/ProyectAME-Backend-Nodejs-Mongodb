85a13f6be6e3d15f4257be14d44588d5331ae8b7
Jesica Velazquez <jesica.velazquez@ibm.com>
2021-03-01 18:56:41 -0300

    fixing bug

--- src/server.js
+++ src/server.js
@@ -18,6 +18,14 @@ app.use(express.json({ extended: true }));
 
 const PORT = process.env.PORT || 4000;
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With, Accept");
+  next();
+});
+
 app.use("/api/usuarios", require("./routes/user_routes"));
 app.use("/api/auth", require("./middleware/auth_routes"));
 app.use("/api/proyectos", require("./routes/proyecto_routes"));
